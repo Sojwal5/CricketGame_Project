b6988d959b376b15ddb79da35b553f8cd472615b
Sojwal <sojwalkuthe@gmail.com>
2024-07-01 20:45:36 +0530

    added object for win loss and draw

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 let computerChoice;
 let userChoice
 let result
+
+let score={
+    win:0,
+    lose:0,
+    Draw:0,
+}
+
 function ComputerChoice_Generate(){
     let random
     random=Math.random()*3;
@@ -21,26 +28,36 @@ function getresult(UserChoice,computerChoice){
     if (UserChoice=='Bat'){
         if (computerChoice=='Bat'){
             result='It is a tie';
+            score.Draw+=1;
         }else if(computerChoice=='Ball'){
             result='user Won';
+            score.win+=1;
         }else{
             result='user lost';
+            score.lose+=1;
         }
     }else if(UserChoice=='Ball'){
         if (computerChoice=='Ball'){
             result='Its a tie';
+            score.Draw+=1;
         }else if(computerChoice=='Bat'){
             result='user lost';
+            score.lose+=1;
         }else{
             result='User won';
+            score.win+=1;
         }
     }else{
         if (computerChoice=='Stump'){
             result='It is a tie';
+            score.Draw+=1;
         }else if(computerChoice=='Ball'){
             result='user Won';
+            score.win+=1;
         }else{
             result='user loss';
+            score.lose+=1;
         }
     }
+    console.log(score);
 }
